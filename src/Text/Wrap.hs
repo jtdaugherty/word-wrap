c6684c7514865c00e05a3af7f5f1abb166d9bceb
Jonathan Daugherty <cygnus@foobox.com>
2017-07-08 11:51:17 -0700

    Add Haddock, expose "unlines" variant, document some logic

--- src/Text/Wrap.hs
+++ src/Text/Wrap.hs
@@ -1,14 +1,21 @@
 module Text.Wrap
   ( wrapText
-  , tokenize
+  , wrapTextToLines
   )
 where
 
 import Data.Char (isSpace)
 import qualified Data.Text as T
 
-wrapText :: Int -> T.Text -> [T.Text]
-wrapText amt s = concat $ wrapLine amt <$> T.lines s
+-- | Wrap text at the specified width. Newlines and whitespace in the
+-- input text are preserved. Returns the lines of text in wrapped form.
+wrapTextToLines :: Int -> T.Text -> [T.Text]
+wrapTextToLines amt s = concat $ wrapLine amt <$> T.lines s
+
+-- | Like wrapTextToLines, but returns the wrapped text reconstructed
+-- with newlines inserted at wrap points.
+wrapText :: Int -> T.Text -> T.Text
+wrapText amt s = T.intercalate (T.pack "\n") $ wrapTextToLines amt s
 
 data Token = WS T.Text | NonWS T.Text
            deriving (Show)
@@ -21,6 +28,7 @@ tokenContent :: Token -> T.Text
 tokenContent (WS t) = t
 tokenContent (NonWS t) = t
 
+-- | Tokenize text into whitespace and non-whitespace chunks.
 tokenize :: T.Text -> [Token]
 tokenize t | T.null t = []
 tokenize t =
@@ -31,7 +39,13 @@ tokenize t =
               else WS leadingWs
     in tok : tokenize (T.drop (tokenLength tok) t)
 
-wrapLine :: Int -> T.Text -> [T.Text]
+-- | Wrap a single line of text into a list of lines that all satisfy
+-- the wrapping width.
+wrapLine :: Int
+         -- ^ The wrapping width.
+         -> T.Text
+         -- ^ A single line of text.
+         -> [T.Text]
 wrapLine limit t =
     let go []     = []
         go [WS _] = []
@@ -44,6 +58,11 @@ wrapLine limit t =
                 Just rest -> firstLineText : go rest
     in go (tokenize t)
 
+-- | Break a token sequence so that all tokens up to but not exceeding
+-- a length limit are included on the left, and if any remain on the
+-- right, return Just those too (or Nothing if there weren't any). If
+-- this breaks a sequence at at point where the next token after the
+-- break point is whitespace, that whitespace token is removed.
 breakTokens :: Int -> [Token] -> ([Token], Maybe [Token])
 breakTokens _ [] = ([], Nothing)
 breakTokens _ [t] = ([t], Nothing)
@@ -51,15 +70,23 @@ breakTokens limit ts =
     -- Take enough tokens until we reach the point where taking more
     -- would exceed the line length.
     let go _ [] = []
+        -- If the line starts with a token that itself exceeds the
+        -- limit, just take that single token as the only one on this
+        -- line.
         go acc (tok:_) | acc == 0 && tokenLength tok > limit = [tok]
+        -- Otherwise take a token if its length plus the accumulator
+        -- doesn't exceed the limit.
         go acc (tok:toks) =
             if tokenLength tok + acc <= limit
             then tok : go (acc + tokenLength tok) toks
             else []
 
+        -- Allowed tokens are the ones we keep on this line.
         allowed = go 0 ts
+        -- The rest go on the next line, to be wrapped again.
         rest = maybeTrim $ drop (length allowed) ts
 
+        -- Trim leading whitespace on wrapped lines.
         maybeTrim [] = []
         maybeTrim (WS _:toks) = toks
         maybeTrim toks = toks
--- tests/Main.hs
+++ tests/Main.hs
@@ -8,16 +8,16 @@ import Text.Wrap
 main :: IO ()
 main = hspec $ do
     it "leaves short lines untouched" $ do
-      wrapText 5 "foo" `shouldBe` ["foo"]
+      wrapTextToLines 5 "foo" `shouldBe` ["foo"]
 
     it "wraps long lines" $ do
-      wrapText 7 "Hello, World!" `shouldBe` ["Hello,", "World!"]
+      wrapTextToLines 7 "Hello, World!" `shouldBe` ["Hello,", "World!"]
 
     it "preserves leading whitespace" $ do
-      wrapText 10 "  Hello, World!" `shouldBe` ["  Hello,", "World!"]
+      wrapTextToLines 10 "  Hello, World!" `shouldBe` ["  Hello,", "World!"]
 
     it "honors preexisting newlines" $ do
-      wrapText 100 "Hello,\nWorld!" `shouldBe` ["Hello,", "World!"]
+      wrapTextToLines 100 "Hello,\nWorld!" `shouldBe` ["Hello,", "World!"]
 
     it "wraps long lines without truncation" $ do
-      wrapText 2 "Hello, World!" `shouldBe` ["Hello,", "World!"]
+      wrapTextToLines 2 "Hello, World!" `shouldBe` ["Hello,", "World!"]
